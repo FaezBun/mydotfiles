d42eab3e721933dd86da836a34bf191207a7d535
FaezBun <depressedshisuii@gmail.com>
2026-02-26 06:11:32 +0800

    Tambah conf untuk Doom Emacs

---
+++ Doom/config.el
@@ -0,0 +1,41 @@
+;; Path : .config/doom 
+;; Path may have change 
+
+;; PLACE your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;;Add-On
+
+;; Appearance
+(add-to-list 'default-frame-alist '(width . 140))  ;; Make it 140 chars wide
+(add-to-list 'default-frame-alist '(height . 50))  ;; Make it 50 lines high
+;; Fullscreen Startup
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
